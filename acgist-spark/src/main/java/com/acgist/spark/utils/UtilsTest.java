801eb1a55d653745cf46a4a0ccf64ea0665acbbf
acgist <289547414@qq.com>
2018-12-19 14:52:03 +0800

    测试代码

---
+++ acgist-spark/src/main/java/com/acgist/spark/utils/UtilsTest.java
@@ -0,0 +1,11 @@
+package com.acgist.spark.utils;
+
+import org.apache.spark.util.Utils;
+
+public class UtilsTest {
+
+	public static void main(String[] args) {
+		System.out.println(Utils.localHostName());
+	}
+	
+}
