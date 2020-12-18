135e1717496fa35c49a1438f1a660058137b9ac1
acgist <289547414@qq.com>
2020-12-18 11:02:37 +0800

    结构优化

--- acgist-share/2018/DownloadServletTest.java
+++ acgist-share/2018/DownloadServletTest.java
@@ -11,7 +11,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.junit.Test;
 
-public class DownloadTest {
+public class DownloadServletTest {
 
 	@Test
 	public void download() {
