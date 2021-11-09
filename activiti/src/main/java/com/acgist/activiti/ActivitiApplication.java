308c9062d36b33d6bf1f1273c3ec559c36ce3d1c
acgist <289547414@qq.com>
2021-11-09 21:10:03 +0800

    webui

--- activiti/src/main/java/com/acgist/activiti/ActivitiApplication.java
+++ activiti/src/main/java/com/acgist/activiti/ActivitiApplication.java
@@ -1,9 +1,15 @@
 package com.acgist.activiti;
 
 import org.activiti.spring.boot.SecurityAutoConfiguration;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@ComponentScan({"org.activiti.rest.editor", "com.acgist.activiti"})
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ActivitiApplication {
 
@@ -11,4 +17,15 @@ public class ActivitiApplication {
 		SpringApplication.run(ActivitiApplication.class, args);
 	}
 
+	@Configurable
+	@EnableWebSecurity
+	public static class AdminConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+				.csrf().disable()
+				.antMatcher("/**").authorizeRequests().anyRequest().permitAll();
+		}
+	}
+	
 }
--- activiti/src/main/java/com/acgist/activiti/ActivitiController.java
+++ activiti/src/main/java/com/acgist/activiti/ActivitiController.java
@@ -1,29 +1,65 @@
-//package com.acgist.activiti;
-//
-//import org.activiti.engine.RuntimeService;
-//import org.activiti.engine.TaskService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//@RequestMapping("/activiti")
-//public class ActivitiController {
-//
-//	@Autowired
-//	private TaskService taskService;
-//	@Autowired
-//    private RuntimeService runtimeService;
-//	
-//	public void start() {
-//		this.runtimeService.startProcessInstanceById("");
-//	}
-//	
-//	public void list() {
-//		this.taskService.createTaskQuery()
-////			.processDefinitionId("") // 任务ID
-//			.taskAssignee("") // 负责人
-//			.listPage(0, 2);
-//	}
-//	
-//}
+package com.acgist.activiti;
+
+import org.activiti.editor.constants.ModelDataJsonConstants;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.repository.Model;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+/**
+ * 流程：列表、添加、修改、删除 用户：开始、执行、列表
+ */
+@Controller
+@RequestMapping("/activiti")
+public class ActivitiController {
+
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	@Autowired
+	private TaskService taskService;
+	@Autowired
+	private RuntimeService runtimeService;
+	@Autowired
+	private RepositoryService repositoryService;
+
+	@RequestMapping("/create")
+	public String create(String name, String description) {
+		if (StringUtils.isEmpty(name)) {
+			return null;
+		}
+		if(StringUtils.isEmpty(description)) {
+			description = name;
+		}
+		String id = null;
+		String key = name;
+		String revision = "1";
+		final Model model = this.repositoryService.newModel();
+		final ObjectNode modelNode = this.objectMapper.createObjectNode();
+		modelNode.put(ModelDataJsonConstants.MODEL_NAME, name);
+		modelNode.put(ModelDataJsonConstants.MODEL_REVISION, revision);
+		modelNode.put(ModelDataJsonConstants.MODEL_DESCRIPTION, description);
+		model.setKey(key);
+		model.setName(name);
+//		model.setCategory(category);
+		model.setMetaInfo(modelNode.toString());
+		this.repositoryService.saveModel(model);
+		id = model.getId();
+		ObjectNode editorNode = this.objectMapper.createObjectNode();
+		editorNode.put("id", "canvas");
+		editorNode.put("resourceId", "canvas");
+		ObjectNode stencilSetNode = this.objectMapper.createObjectNode();
+		stencilSetNode.put("namespace", "http://b3mn.org/stencilset/bpmn2.0#");
+		editorNode.put("stencilset", stencilSetNode);
+		this.repositoryService.addModelEditorSource(id, editorNode.toString().getBytes());
+		return "redirect:/modeler.html?modelId=" + id;
+	}
+
+}
