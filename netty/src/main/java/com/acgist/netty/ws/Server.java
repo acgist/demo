fa367e36c636f4be789e3fdffe0627421193e2c1
acgist <289547414@qq.com>
2021-12-17 14:57:37 +0800

    [#] 搞不定这个异常
    使用机构颁发的证书没有问题，自己的证书不管怎么添加受信任都会提示错误。

--- netty/src/main/java/com/acgist/netty/ws/Server.java
+++ netty/src/main/java/com/acgist/netty/ws/Server.java
@@ -3,126 +3,113 @@ package com.acgist.netty.ws;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.security.KeyStore;
-import java.security.PublicKey;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
+import java.util.List;
 
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLEngine;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
-import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.MessageToMessageCodec;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.logging.LoggingHandler;
 import io.netty.handler.ssl.SslHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
 
+/**
+ * Netty WebSocket
+ * 
+ * 注意事项：
+ * 首先需要访问https://localhost:8080受到信任
+ * 然后定义：var socket = new WebSocket('wss://localhost:8080/ws')
+ * 不知为何报错：Received fatal alert: certificate_unknown（需要真实机构颁发证书没有问题）
+ * 这个异常没有影响：socket.send('acgist')
+ * 
+ * @author yusheng
+ */
 public class Server {
 
-	public static void main(String[] args) throws Exception {
-		
-		KeyStore ks = KeyStore.getInstance("JKS");
-		InputStream ksInputStream = Server.class.getResourceAsStream("/acgist.jks");
-		ks.load(ksInputStream, "123456".toCharArray());
-		ks.aliases().asIterator().forEachRemaining(aliase -> {
-			try {
-				PublicKey publicKey = ks.getCertificate(aliase).getPublicKey();
-				System.out.println(publicKey);
-				System.out.println(ks.getKey(aliase, "123456".toCharArray()));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		});
-		
-		ServerBootstrap serverBootstrap = new ServerBootstrap();
-		NioEventLoopGroup bossGroup = new NioEventLoopGroup();
-		NioEventLoopGroup workGroup = new NioEventLoopGroup();
-		serverBootstrap.group(bossGroup, workGroup);
-		serverBootstrap.channel(NioServerSocketChannel.class);
-		serverBootstrap.childHandler(new NioWebSocketChannelInitializer());
-		Channel channel = serverBootstrap.bind(8080).sync().channel();
-		channel.closeFuture().sync();
-	}
+    public static void main(String[] args) throws Exception {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workGroup = new NioEventLoopGroup();
+        serverBootstrap.group(bossGroup, workGroup);
+        serverBootstrap.channel(NioServerSocketChannel.class);
+        serverBootstrap.childHandler(new NioWebSocketChannelInitializer());
+        Channel channel = serverBootstrap.bind(8080).sync().channel();
+        channel.closeFuture().sync();
+    }
 
-	public static class NioWebSocketChannelInitializer extends ChannelInitializer<SocketChannel> {
-		@Override
-		protected void initChannel(SocketChannel channel) throws Exception {
-			channel.pipeline().addLast("logging", new LoggingHandler("DEBUG"));
-			channel.pipeline().addLast("http-codec", new HttpServerCodec());
-			channel.pipeline().addLast("aggregator", new HttpObjectAggregator(65536));
-			channel.pipeline().addLast("http-chunked", new ChunkedWriteHandler());
-			channel.pipeline().addLast("handler", new NioWebSocketHandler());
-			channel.pipeline().addLast("ws", new WebSocketServerProtocolHandler("/ws"));
-			// 以下为要支持wss所需处理
-			KeyStore ks = KeyStore.getInstance("JKS");
-			InputStream ksInputStream = this.getClass().getResourceAsStream("/acgist.jks");
-			ks.load(ksInputStream, "123456".toCharArray());
-			ks.aliases().asIterator().forEachRemaining(System.out::println);
-			KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-			kmf.init(ks, "123456".toCharArray());
-			SSLContext sslContext = SSLContext.getInstance("TLS");
-			sslContext.init(kmf.getKeyManagers(), new TrustManager[] {
-					new X509TrustManager() {
+    public static class NioWebSocketChannelInitializer extends ChannelInitializer<SocketChannel> {
+        @Override
+        protected void initChannel(SocketChannel channel) throws Exception {
+            // 需把SslHandler添加首位
+            InputStream input = this.getClass().getResourceAsStream("/ca.jks");
+            KeyStore keyStore = KeyStore.getInstance("JKS");
+            keyStore.load(input, "123456".toCharArray());
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            keyManagerFactory.init(keyStore, "123456".toCharArray());
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+//            sslContext.init(keyManagerFactory.getKeyManagers(), null, new SecureRandom());
+            sslContext.init(keyManagerFactory.getKeyManagers(), null, null);
+            SSLEngine sslEngine = sslContext.createSSLEngine();
+            sslEngine.setUseClientMode(false);
+            sslEngine.setNeedClientAuth(false);
+//            sslEngine.setEnabledProtocols(new String[] {"TLSv1.2", "TLSv1.1"});
+            channel.pipeline().addFirst(new SslHandler(sslEngine));
+            
+//            SslContext sslCtx = SslContextBuilder.forServer(keyManagerFactory).build();
+//            channel.pipeline().addLast(sslCtx.newHandler(channel.alloc()));
+            
+            channel.pipeline().addLast("logging", new LoggingHandler("DEBUG"));
+            channel.pipeline().addLast(new HttpServerCodec());
+            channel.pipeline().addLast(new ChunkedWriteHandler());
+            channel.pipeline().addLast(new HttpObjectAggregator(65536));
+            channel.pipeline().addLast(new WebSocketServerProtocolHandler("/ws"));
+//            channel.pipeline().addLast(new WebSocketServerCompressionHandler());
+//            channel.pipeline().addLast(new WebSocketServerProtocolHandler("/ws", null, true));
+            channel.pipeline().addLast(new NioWebSocketHandler());
+        }
+    }
 
-						@Override
-						public void checkClientTrusted(X509Certificate[] chain, String authType)
-								throws CertificateException {
-							
-						}
+    public static class NioWebSocketHandler extends MessageToMessageCodec<WebSocketFrame, String> {
 
-						@Override
-						public void checkServerTrusted(X509Certificate[] chain, String authType)
-								throws CertificateException {
-							
-						}
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            System.out.println("连接");
+        }
 
-						@Override
-						public X509Certificate[] getAcceptedIssuers() {
-							return null;
-						}
-						
-					}
-			}, null);
-			SSLEngine sslEngine = sslContext.createSSLEngine();
-			sslEngine.setUseClientMode(false);
-			sslEngine.setNeedClientAuth(false);
-			// 需把SslHandler添加在第一位
-			channel.pipeline().addFirst("ssl", new SslHandler(sslEngine));
+        @Override
+        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+            System.out.println("断开");
+        }
 
-		}
-	}
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+//            cause.printStackTrace();
+        }
 
-	public static class NioWebSocketHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
-		@Override
-		public void channelActive(ChannelHandlerContext ctx) throws Exception {
-			System.out.println("连接");
-		}
+        @Override
+        protected void encode(ChannelHandlerContext ctx, String msg, List<Object> out) throws Exception {
+            out.add(new TextWebSocketFrame(msg));
+        }
 
-		@Override
-		public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-			System.out.println("断开");
-		}
+        @Override
+        protected void decode(ChannelHandlerContext ctx, WebSocketFrame msg, List<Object> out) throws Exception {
+            System.out.println(msg.content().toString(StandardCharsets.UTF_8));
+            out.add(msg.content().toString(StandardCharsets.UTF_8));
+        }
 
-		@Override
-		public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-			cause.printStackTrace();
-		}
+    }
 
-		@Override
-		protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
-			System.out.println(msg.content().toString(StandardCharsets.UTF_8));
-		}
-	}
 }
