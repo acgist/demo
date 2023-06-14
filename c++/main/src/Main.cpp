fb0a93b742e65820991f075a17327f6ecc1a884a
acgist <289547414@qq.com>
2023-06-14 09:09:12 +0800

    [+] pytorch学习

--- c++/main/src/Main.cpp
+++ c++/main/src/Main.cpp
@@ -15,5 +15,13 @@ int main(int argc, char **argv) {
 	system("chcp 65001");
 	std::cout << "Hello World" << std::endl;
 	std::cout << "中文测试" << std::endl;
+	int  a{100};
+	int  b(101);
+	long c{100L};
+	long d;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << c << std::endl;
+	std::cout << d << std::endl;
 	return 0;
 }
